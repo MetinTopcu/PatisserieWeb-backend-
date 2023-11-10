ecd4de7e6d188e67634a79a785997f807bc7ba95
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-10 16:52:58 +0300

    Automapper

---
+++ Services/Catalog/Patisserie.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Patisserie.Services.Catalog.Dtos;
+using Patisserie.Services.Catalog.Models;
+
+namespace Patisserie.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Product, ProductCreateDto>().ReverseMap();
+            CreateMap<Product, ProductUpdateDto>().ReverseMap();
+        }
+    }
+}
