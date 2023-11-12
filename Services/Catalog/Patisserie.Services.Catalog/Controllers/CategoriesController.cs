3973b9128900d91054f1aebdb999a058ec46ed6e
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-12 15:18:10 +0300

    CategoriesController and ProductsController

---
+++ Services/Catalog/Patisserie.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Patisserie.Services.Catalog.Dtos;
+using Patisserie.Services.Catalog.Services;
+using Patisserie.Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace Patisserie.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        internal CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(categories);  
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id) 
+        {
+            var categories = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(categories);
+        }
+
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+           var response =  await _categoryService.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ Services/Catalog/Patisserie.Services.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Patisserie.Services.Catalog.Dtos;
+using Patisserie.Services.Catalog.Services;
+using Patisserie.Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace Patisserie.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class ProductsController : CustomBaseController
+    {
+        private readonly IProductService _productService;
+
+        internal ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _productService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _productService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _productService.GetAllByUserIdAsync(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateDto productCreateDto)
+        {
+            var response = await _productService.CreateAsync(productCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productUpdateDto)
+        {
+            var response = await _productService.UpdateAsync(productUpdateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _productService.DeleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+
+    }
+}
