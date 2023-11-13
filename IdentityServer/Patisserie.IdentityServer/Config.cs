eadac7c5939f23c3559c127d2700eb8a71096696
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-13 18:03:18 +0300

    IdentityResources at Config

--- IdentityServer/Patisserie.IdentityServer/Config.cs
+++ IdentityServer/Patisserie.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Patisserie.IdentityServer
@@ -20,6 +21,10 @@ namespace Patisserie.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){Name ="roles",DisplayName="Roles",Description="Kullanıcı Rolleri",UserClaims=new[]{"role"}}
                 
                    };
 
@@ -42,7 +47,25 @@ namespace Patisserie.IdentityServer
                     AllowedGrantTypes=GrantTypes.ClientCredentials,
                     AllowedScopes={ "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
 
+                },
+                new Client
+                {
+                    ClientName="Asp.Net Core MVC",
+                    ClientId ="WebMvcClientForUser",
+                    AllowOfflineAccess=true,
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage=TokenUsage.ReUse
+
                 }
+
             };
     }
 }
