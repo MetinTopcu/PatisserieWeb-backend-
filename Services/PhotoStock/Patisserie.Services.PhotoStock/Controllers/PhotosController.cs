278bb114191ece7f4378cc1dcde9508e4c9b0353
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-13 19:12:47 +0300

    PhotoStockApi and PhotoController

---
+++ Services/PhotoStock/Patisserie.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Patisserie.Services.PhotoStock.Dtos;
+using Patisserie.Shared.ControllerBases;
+using Patisserie.Shared.Dtos;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Patisserie.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if(photo !=null && photo.Length> 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream,cancellationToken);
+                }
+
+                //http://www.photostock.api.com/photos/asdfsh.jpg
+                var returnPath = "photos" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+    }
+}
