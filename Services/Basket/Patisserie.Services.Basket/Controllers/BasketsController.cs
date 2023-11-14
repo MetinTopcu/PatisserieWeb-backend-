bdf374f6482871d02425fa5343c34ac94e956d12
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-14 18:03:00 +0300

    Added BasketsController and Scoped in Startup

---
+++ Services/Basket/Patisserie.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Patisserie.Services.Basket.Dtos;
+using Patisserie.Services.Basket.Services;
+using Patisserie.Shared.ControllerBases;
+using Patisserie.Shared.Services;
+using System.Threading.Tasks;
+
+namespace Patisserie.Services.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            var response = await _basketService.GetBasket(_sharedIdentityService.GetUserId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrUpdateBasket(BasketDto basketdto)
+        {
+            var response = await _basketService.SaveOrUpdate(basketdto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            var response = await _basketService.Delete(_sharedIdentityService.GetUserId);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
