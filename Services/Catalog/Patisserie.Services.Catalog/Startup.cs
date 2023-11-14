13b3b4083976b30bd0a2c3f5d8f6397025088b65
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-14 16:30:02 +0300

    Added BasketDto,BasketItemDto and RedisSettings

--- Services/Catalog/Patisserie.Services.Catalog/Startup.cs
+++ Services/Catalog/Patisserie.Services.Catalog/Startup.cs
@@ -31,7 +31,6 @@ namespace Patisserie.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.Authority = Configuration["IdentityServerURL"];
