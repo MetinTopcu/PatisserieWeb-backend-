a379651df4e4f9b89cd46b0bc734ea663959f4b0
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-18 22:54:48 +0300

    Created Dto and Automapper

---
+++ Services/Order/Patisserie.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patisserie.Services.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string ProductId { get; private set; }
+
+        public string ProductName { get; private set; }
+
+        public string PictureUrl { get; private set; }
+
+        public Decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+    }
+}
